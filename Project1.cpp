65c8a6a372db8ffe9d1a013fd556902e8544d6fb
ENG-MOSTAFA24 <mo.mohamedpro1@gmail.com>
2026-09-16 21:42:41 +0300

    Add Project Files

---
+++ Project1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	clsDblLinkedList <int> MydblLinkedList;
+
+	MydblLinkedList.InsertAtBeginning(5);
+	MydblLinkedList.InsertAtBeginning(4);
+	MydblLinkedList.InsertAtBeginning(3);
+	MydblLinkedList.InsertAtBeginning(2);
+	MydblLinkedList.InsertAtBeginning(1);
+
+
+	cout << "Linked List Content : " << endl;
+	MydblLinkedList.PrintList();
+	
+
+
+	system("pause>0");
+	return  0;
+}
